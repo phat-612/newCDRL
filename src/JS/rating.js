b9074c104a57dd43ae1db236b5a806bde6cbdb32
dawn1810 <106864922+dawn1810@users.noreply.github.com>
2023-07-29 23:16:55 +0700

    Phat tinh tam

--- src/JS/rating.js
+++ src/JS/rating.js
@@ -1,28 +1,9 @@
-function Setting_pageWrapper_drop() {
-  for (const key of Setting_pageWrapper) {
-    key.style.display = "none";
-  }
-}
-
-function Setting_item_drop() {
-  for (const key of Setting_item) {
-    key.style.backgroundColor = "transparent";
-  }
-}
-
-function page5_composed_drop() {
-  // ẩn page5_composed
-  for (const key of page5_composed) {
-    key.style.display = "none";
-  }
-}
-
-function page5_a_up_drop() {
-  for (const key of page5_a_up) {
-    key.style.display = "none";
-  }
-}
 $(document).ready(function () {
+  const $dropArea = $('.modal_wrap_img_item');
+  const $uploadInput = $('.upload-input');
+  const $uploadButton = $('.up-img-btn');
+  const $uploadedImage = $('.up-img');
+
   $(".up-btn").click(function () {
     $(".modal").show();
   });
@@ -30,6 +11,52 @@ $(document).ready(function () {
   $(".close-dialog").click(function () {
     $(".modal").hide();
   });
+
+  $(".add-btn").click(function () {
+    $(".modal").hide();
+  });
+
+  // Add event listeners
+  $dropArea.on('dragover', handleDragOver);
+  $dropArea.on('dragleave', handleDragLeave);
+  $dropArea.on('drop', handleDrop);
+  $uploadButton.on('click', handleUploadButtonClick);
+  $uploadInput.on('change', handleUploadInputChange);
+
+  function handleDragOver(event) {
+    event.preventDefault();
+    $dropArea.addClass('dragover');
+  }
+
+  function handleDragLeave(event) {
+    event.preventDefault();
+    $dropArea.removeClass('dragover');
+  }
+
+  function handleDrop(event) {
+    event.preventDefault();
+    $dropArea.removeClass('dragover');
+    const file = event.originalEvent.dataTransfer.files[0];
+    displayImage(file);
+  }
+
+  function handleUploadButtonClick() {
+    $uploadInput.click();
+  }
+
+  function handleUploadInputChange(event) {
+    const file = event.target.files[0];
+    displayImage(file);
+  }
+
+  function displayImage(file) {
+    const reader = new FileReader();
+    reader.onload = function (event) {
+      $uploadedImage.attr('src', event.target.result);
+    }
+    reader.readAsDataURL(file);
+    $('.up-img-btn i').hide();
+  }
 });
 
 $(document).mouseup(function (e) {
@@ -40,3 +67,64 @@ $(document).mouseup(function (e) {
     container.hide();
   }
 });
+
+// check box ----------------------------------------------------------------------------------------------------
+
+const checkbox_list = {
+  tier_1: ['morning1', 'morning2'],
+  tier_2: ['morning3', 'morning4', 'morning5', 'morning6'],
+  tier_3: ['morning7', 'morning8'],
+  tier_4: ['morning9', 'morning10'],
+  tier_5: ['morning11', 'morning12'],
+  tier_6: ['morning13', 'morning14'],
+};
+
+const select_list = {
+  tier_1: 'mySelect3',
+  tier_2: '',
+  tier_3: 'mySelect5',
+  tier_4: 'mySelect6',
+  tier_5: 'mySelect7',
+  tier_6: 'mySelect16',
+}
+
+
+
+
+
+function handleCheckboxChange(event) {
+  const checkbox = event.target;
+  console.log(checkbox);
+  if (checkbox.checked) {
+    for (const [tier, checkboxTier] of Object.entries(checkbox_list)) {
+      if (checkboxTier.includes(checkbox.id)) {
+        const selectElement = document.getElementById(select_list[tier]);
+        console.log(selectElement);
+        selectElement.value = parseInt(checkbox.value);
+        checkboxTier.forEach(checkbox_i => {
+          if (checkbox.id != checkbox_i) {
+            document.getElementById(checkbox_i).checked = false;
+          }
+        });
+      }
+    }
+
+
+    // Object.values(checkbox_list).forEach(checkboxTier => {
+    //   if (checkboxTier.includes(checkbox.id)) {
+    //     checkboxTier.forEach(checkbox_i => {
+    //       if (checkbox.id != checkbox_i) {
+    //         document.getElementById(checkbox_i).checked = false;
+    //       }
+
+    //     });
+    //   }
+    // });
+  }
+}
+
+const checkboxes = document.querySelectorAll('.checkbox-wrapper-4 input[type="checkbox"]');
+checkboxes.forEach(checkbox => {
+  checkbox.addEventListener('change', handleCheckboxChange);
+});
+
