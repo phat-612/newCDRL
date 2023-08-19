09129d9617cefcf7e231ded304c2f9a691e34cd1
Ruri Meiko <chandoralong@gmail.com>
2023-08-19 21:58:38 +0700

    Co-authored-by: Hung-Thinhh <Hung-Thinhh@users.noreply.github.com> Co-authored-by: dragonccm <dragonccm@users.noreply.github.com> Co-authored-by: dawn1810 <dawn1810@users.noreply.github.com>

---
+++ src/JS/doankhoa-manage-teacher.js
@@ -0,0 +1,26 @@
+$("#edit__class").click(function () {
+  $(".modal.edit").show();
+});
+
+$(".modal.edit").click(function () {
+  $(".modal.edit").hide();
+});
+
+$(".modal_wrap.edit").click(function (e) {
+  e.stopPropagation();
+});
+
+$("#add__class").click(function () {
+  $(".modal.add").show();
+});
+
+$(".modal.add").click(function () {
+  $(".modal.add").hide();
+});
+$(".modal_wrap.add").click(function (e) {
+  e.stopPropagation();
+});
+$(document).on("click", "exist_btn", async function () {
+  
+  $(".modal.edit").hide();
+})
