17a7e993a6e26e3057d91a6abb1bf0cfb0003560
dawn1810 <binhminh19112003@gmail.com>
2024-07-04 22:43:01 +0700

    a

--- src/JS/doankhoa-manage-activities.js
+++ src/JS/doankhoa-manage-activities.js
@@ -6,6 +6,16 @@ tippy('#dep_access_link', {
     content: 'Truy cập đường dẫn',
 });
 
+function generateUUID() {
+    // Hàm tạo chuỗi UUID
+    // Tham khảo: https://stackoverflow.com/a/2117523/13347726
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (Math.random() * 16) | 0,
+            v = c === 'x' ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+    });
+}
+
 let atv_id = generateUUID(); // fake id to know a new id
 let curr_edit = undefined;
 
